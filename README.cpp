e5b6b9bbce46c24b5d52138304ca52a2b6894c14
siyagumber04 <siyagumber6@gmail.com>
2025-04-02 18:33:19 +0530

    Add files via upload

---
+++ README.cpp
@@ -0,0 +1,1 @@
+A login and registration system using C++. The system allows new users to register by providing their personal information such as first name, last name, email address, and password. The system validates the input data to ensure its correctness. Existing users can log in using their username and password. 
